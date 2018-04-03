715a5a59551022afdefc84a96ac6f7b7859040f1
Unknown <asjchae@gmail.com>
2018-04-02 17:44:16 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+// Helper functions for accessing the Reddit API.
+var https = require('https');
+var Parse = require('parse/node').Parse;
+
+function validateAuthToken(id, token) {
+    return request("me", token)
+        .then((response) => {
+            if (response && response.id == id) {
+                return;
+            }
+            throw new Parse.Error(
+                Parse.Error.OBJECT_NOT_FOUND,
+                'Reddit auth is invalid for this user.');
+        });
+}
+
+// Returns a promise that fulfills if this user id is valid.
+function validateAuthData(authData) {
+    return validateAuthToken(authData.id, authData.access_token).then(() => {
+        // Validation with auth token worked
+        return;
+    });
+}
+
+// Returns a promise that fulfills if this app id is valid.
+function validateAppId() {
+    return Promise.resolve();
+}
+
+// A promisey wrapper for api requests
+function request(path, token) {
+    return new Promise(function(resolve, reject) {
+        https.get({
+            hostname: "oauth.reddit.com",
+            path: "/api/v1/" + path,
+            headers: {
+                Authorization: 'bearer ' + token,
+                'User-Agent': 'Gif Your Game'
+            }
+        }, function(res) {
+            var data = '';
+            res.on('data', function(chunk) {
+                data += chunk;
+            });
+            res.on('end', function() {
+                try {
+                    data = JSON.parse(data);
+                } catch(e) {
+                    return reject(e);
+                }
+                resolve(data);
+            });
+        }).on('error', function() {
+            reject('Failed to validate this access token with Reddit.');
+        });
+    });
+}
+
+module.exports = {
+    validateAppId: validateAppId,
+    validateAuthData: validateAuthData
+};
